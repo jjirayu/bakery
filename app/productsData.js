23b2d505ded2335a05c6ebf8ca3f7c600487c4d4
jjirayu <jjirayu@hotmail.com>
2024-07-24 14:40:11 +0700

    master

--- app/productsData.js
+++ app/productsData.js
@@ -9,6 +9,14 @@ const initialProducts = [
     { name: 'ชาตรามือ ฉลากแดง 400 ก.', price: '74฿', quantity: 1, image: '/images/ชาตรามือแดง.jpeg' },
     { name: 'ชาตรามือ ชาเขียว 200 ก.', price: '74฿', quantity: 1, image: '/images/ชาตรามือเขียว.jpeg' },
     { name: 'ชาตรามือ ฉลากทอง 400 ก.', price: '110฿', quantity: 1, image: '/images/ชาตรามือทอง.jpeg' },
+    { name: 'ฮีโร่ ถุงขยะดำหนา 28x36.', price: '70฿', quantity: 1, image: '/images/ฮีโร่ถุงขยะหนา 28x36.jpeg' },
+    { name: 'ฮีโร่ ถุงขยะดำหนา 24x30 1 กก.', price: '70฿', quantity: 1, image: '/images/ฮีโร่ถุงขยะหนา 24x30 1 กก.jpeg' },
+    { name: 'ฮีโร่ถุงขยะหนา 30x40 50 ถุง.', price: '70฿', quantity: 1, image: '/images/ฮีโร่ถุงขยะหนา 30x40 50 ถุง.jpeg' },
+    { name: 'แก้ววิสกี้ Ocean 340 ml', price: '35฿', quantity: 1, image: '/images/แก้ววิสกี้ Ocean 340 ml.jpg' },
+    { name: 'กระทะจรวด 36 ซม.', price: '289฿', quantity: 1, image: '/images/กระทะจรวด 36.jpg' },
+    { name: 'แก้วน้ำ Ocean 245 ml', price: '10฿', quantity: 1, image: '/images/แก้วน้ำ High ball 245 ml.jpg' },
+    { name: 'แก้วน้ำ Ocean Patio 290 ml', price: '20฿', quantity: 1, image: '/images/PATIO HI BALL 290 ml.jpg' },
+    { name: 'แก้วน้ำ Ocean CHARISMA LONG DRINK 415 ml', price: '35฿', quantity: 1, image: '/images/CHARISMA LONG DRINK.jpg' },
   ];
   
   export default initialProducts;
